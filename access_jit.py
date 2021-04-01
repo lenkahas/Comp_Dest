e3f1c2dc5fe909f240d596c412e01142404a17bc
ljwolf <levi.john.wolf@gmail.com>
2021-04-01 15:55:02 +0100

    add and finish rearranging

--- access_jit.py
+++ access_jit.py
@@ -1,5 +1,4 @@
 import numpy, pandas
-import data
 from sklearn.preprocessing import LabelEncoder
 
 try:
@@ -13,7 +12,7 @@ except ImportError:
         return decorator
 
 
-def access_numba(flows, filter_flow=False, n_jobs=-1):
+def access(flows, filter_flow=False, n_jobs=-1):
 
     origins = flows.origin.unique()
     destinations = flows.destination.unique()
@@ -98,4 +97,39 @@ def _access_numba_i(flowix, origins, destinations, all_masses, weights, distance
 
 
 if __name__ == "__main__":
-    access_numba(data.toy())
+    import data
+    a1 = access(data.toy())
+
+    numpy.testing.assert_array_equal(
+        a1.accessibility,
+        [
+            0,  # A_aa is always zero
+            30 * 20,  # A_ab is mass of c times distance from b to c
+            25 * 20,  # A_ac is mass of b times distance from c to b
+            30 * 10,  # A_ba is mass of c times distance from a to c
+            0,  # A_bb is always zero
+            60 * 10,  # A_bc is mass of a times the distance from c to a
+            25 * 2,  # A_ca is mass of b times distance from a to b
+            60 * 2,  # A_cb is mass of a times distance from b to a
+            0,  # A_cc is always zero
+        ],
+    )
+    print("passed no filter.")
+    a2 = access_slow(data.toy(), filter_flow=True)
+
+    numpy.testing.assert_array_equal(
+        a2.accessibility,
+        [
+            0,  # A_aa is always zero
+            30 * 20,  # A_ab is mass of c times distance from b to c if flow a -> c
+            25 * 20 * 0,  # A_ac is mass of b times distance from c to b if flow a -> b
+            30 * 10,  # A_ba is mass of c times distance from a to c if flow b -> c
+            0,  # A_bb is always zero
+            60 * 10,  # A_bc is mass of a times the distance from c to a if flow b -> a
+            25 * 2,  # A_ca is mass of b times distance from c to b if flow c -> b
+            60 * 10 * 0,  # A_cb has no routes from c to a if flow c -> a
+            0,  # A_cc is always zero
+        ],
+    )
+    print("passed with filter.")
+
